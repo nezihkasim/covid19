0e10d54c034b3ec3c560de8b81a2ad640a210fe3
nezih <nezihkasim@gmail.com>
2020-09-27 04:41:54 +0300

    initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,287 @@
+﻿namespace final_q2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.textBox_country = new System.Windows.Forms.TextBox();
+            this.label_country = new System.Windows.Forms.Label();
+            this.label_confirmed = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox_maxConfirmed = new System.Windows.Forms.TextBox();
+            this.textBox_minConfirmed = new System.Windows.Forms.TextBox();
+            this.button_show = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox_minDeaths = new System.Windows.Forms.TextBox();
+            this.textBox_maxDeaths = new System.Windows.Forms.TextBox();
+            this.textBox_minRecovered = new System.Windows.Forms.TextBox();
+            this.textBox_maxRecovered = new System.Windows.Forms.TextBox();
+            this.textBox_minActive = new System.Windows.Forms.TextBox();
+            this.max_Active = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(3, 161);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(985, 328);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // textBox_country
+            // 
+            this.textBox_country.Location = new System.Drawing.Point(3, 76);
+            this.textBox_country.Name = "textBox_country";
+            this.textBox_country.Size = new System.Drawing.Size(82, 20);
+            this.textBox_country.TabIndex = 1;
+            // 
+            // label_country
+            // 
+            this.label_country.AutoSize = true;
+            this.label_country.BackColor = System.Drawing.SystemColors.Control;
+            this.label_country.Location = new System.Drawing.Point(3, 60);
+            this.label_country.Name = "label_country";
+            this.label_country.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.label_country.Size = new System.Drawing.Size(82, 13);
+            this.label_country.TabIndex = 2;
+            this.label_country.Text = "Country/Region";
+            // 
+            // label_confirmed
+            // 
+            this.label_confirmed.AutoSize = true;
+            this.label_confirmed.Location = new System.Drawing.Point(126, 30);
+            this.label_confirmed.Name = "label_confirmed";
+            this.label_confirmed.Size = new System.Drawing.Size(86, 13);
+            this.label_confirmed.TabIndex = 3;
+            this.label_confirmed.Text = "Confirmed Cases";
+            this.label_confirmed.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(257, 30);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Deaths";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(341, 30);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(60, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Recovered";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(419, 30);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(37, 13);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Active";
+            // 
+            // textBox_maxConfirmed
+            // 
+            this.textBox_maxConfirmed.ForeColor = System.Drawing.SystemColors.InfoText;
+            this.textBox_maxConfirmed.Location = new System.Drawing.Point(129, 46);
+            this.textBox_maxConfirmed.Name = "textBox_maxConfirmed";
+            this.textBox_maxConfirmed.Size = new System.Drawing.Size(83, 20);
+            this.textBox_maxConfirmed.TabIndex = 12;
+            this.textBox_maxConfirmed.Text = "99999999";
+            // 
+            // textBox_minConfirmed
+            // 
+            this.textBox_minConfirmed.ForeColor = System.Drawing.SystemColors.InfoText;
+            this.textBox_minConfirmed.Location = new System.Drawing.Point(129, 76);
+            this.textBox_minConfirmed.Name = "textBox_minConfirmed";
+            this.textBox_minConfirmed.Size = new System.Drawing.Size(83, 20);
+            this.textBox_minConfirmed.TabIndex = 13;
+            this.textBox_minConfirmed.Text = "0";
+            this.textBox_minConfirmed.TextChanged += new System.EventHandler(this.textBox_maxConfirmed_TextChanged);
+            // 
+            // button_show
+            // 
+            this.button_show.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button_show.Location = new System.Drawing.Point(405, 113);
+            this.button_show.Name = "button_show";
+            this.button_show.Size = new System.Drawing.Size(244, 48);
+            this.button_show.TabIndex = 14;
+            this.button_show.Text = "SHOW";
+            this.button_show.UseVisualStyleBackColor = true;
+            this.button_show.Click += new System.EventHandler(this.button_show_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.label1.Location = new System.Drawing.Point(100, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(26, 13);
+            this.label1.TabIndex = 15;
+            this.label1.Text = "max";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.label2.Location = new System.Drawing.Point(100, 79);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(23, 13);
+            this.label2.TabIndex = 16;
+            this.label2.Text = "min";
+            // 
+            // textBox_minDeaths
+            // 
+            this.textBox_minDeaths.ForeColor = System.Drawing.SystemColors.InfoText;
+            this.textBox_minDeaths.Location = new System.Drawing.Point(237, 76);
+            this.textBox_minDeaths.Name = "textBox_minDeaths";
+            this.textBox_minDeaths.Size = new System.Drawing.Size(63, 20);
+            this.textBox_minDeaths.TabIndex = 18;
+            this.textBox_minDeaths.Text = "0";
+            // 
+            // textBox_maxDeaths
+            // 
+            this.textBox_maxDeaths.ForeColor = System.Drawing.SystemColors.InfoText;
+            this.textBox_maxDeaths.Location = new System.Drawing.Point(237, 46);
+            this.textBox_maxDeaths.Name = "textBox_maxDeaths";
+            this.textBox_maxDeaths.Size = new System.Drawing.Size(63, 20);
+            this.textBox_maxDeaths.TabIndex = 17;
+            this.textBox_maxDeaths.Text = "99999999";
+            // 
+            // textBox_minRecovered
+            // 
+            this.textBox_minRecovered.ForeColor = System.Drawing.SystemColors.InfoText;
+            this.textBox_minRecovered.Location = new System.Drawing.Point(326, 76);
+            this.textBox_minRecovered.Name = "textBox_minRecovered";
+            this.textBox_minRecovered.Size = new System.Drawing.Size(63, 20);
+            this.textBox_minRecovered.TabIndex = 20;
+            this.textBox_minRecovered.Text = "0";
+            // 
+            // textBox_maxRecovered
+            // 
+            this.textBox_maxRecovered.ForeColor = System.Drawing.SystemColors.InfoText;
+            this.textBox_maxRecovered.Location = new System.Drawing.Point(326, 46);
+            this.textBox_maxRecovered.Name = "textBox_maxRecovered";
+            this.textBox_maxRecovered.Size = new System.Drawing.Size(63, 20);
+            this.textBox_maxRecovered.TabIndex = 19;
+            this.textBox_maxRecovered.Text = "99999999";
+            // 
+            // textBox_minActive
+            // 
+            this.textBox_minActive.ForeColor = System.Drawing.SystemColors.InfoText;
+            this.textBox_minActive.Location = new System.Drawing.Point(405, 76);
+            this.textBox_minActive.Name = "textBox_minActive";
+            this.textBox_minActive.Size = new System.Drawing.Size(68, 20);
+            this.textBox_minActive.TabIndex = 22;
+            this.textBox_minActive.Text = "0";
+            // 
+            // max_Active
+            // 
+            this.max_Active.ForeColor = System.Drawing.SystemColors.InfoText;
+            this.max_Active.Location = new System.Drawing.Point(405, 46);
+            this.max_Active.Name = "max_Active";
+            this.max_Active.Size = new System.Drawing.Size(68, 20);
+            this.max_Active.TabIndex = 21;
+            this.max_Active.Text = "99999999";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(12, 145);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(243, 13);
+            this.label6.TabIndex = 23;
+            this.label6.Text = "*Sort by Clicking the Column Headers in Datatable";
+            this.label6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(991, 492);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBox_minActive);
+            this.Controls.Add(this.max_Active);
+            this.Controls.Add(this.textBox_minRecovered);
+            this.Controls.Add(this.textBox_maxRecovered);
+            this.Controls.Add(this.textBox_minDeaths);
+            this.Controls.Add(this.textBox_maxDeaths);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button_show);
+            this.Controls.Add(this.textBox_minConfirmed);
+            this.Controls.Add(this.textBox_maxConfirmed);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label_confirmed);
+            this.Controls.Add(this.label_country);
+            this.Controls.Add(this.textBox_country);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox textBox_country;
+        private System.Windows.Forms.Label label_country;
+        private System.Windows.Forms.Label label_confirmed;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox_maxConfirmed;
+        private System.Windows.Forms.TextBox textBox_minConfirmed;
+        private System.Windows.Forms.Button button_show;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox_minDeaths;
+        private System.Windows.Forms.TextBox textBox_maxDeaths;
+        private System.Windows.Forms.TextBox textBox_minRecovered;
+        private System.Windows.Forms.TextBox textBox_maxRecovered;
+        private System.Windows.Forms.TextBox textBox_minActive;
+        private System.Windows.Forms.TextBox max_Active;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace final_q2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            DataTable dataTable = new DataTable();
+
+            dataTable.Columns.Add("Country");                               // 0
+            dataTable.Columns.Add("Confirmed", typeof(int));                // 1
+            dataTable.Columns.Add("Deaths", typeof(int));                   // 2
+            dataTable.Columns.Add("Recovered", typeof(int));                // 3
+            dataTable.Columns.Add("Active", typeof(int));                   // 4
+            dataTable.Columns.Add("New Cases", typeof(int));                // 5
+            dataTable.Columns.Add("New Deaths", typeof(int));               // 6
+            dataTable.Columns.Add("New Recovered", typeof(int));            // 7
+            dataTable.Columns.Add("Deaths/100 cases");                      // 8   Tried to define as double, but couldn't parse the data due to its style (different culture)
+            dataTable.Columns.Add("Recovered/100 cases");                   // 9   Tried to define as double, but couldn't parse the data due to its style (different culture)
+            dataTable.Columns.Add("Deaths/100 Recovered");                  // 10  Tried to define as double, but couldn't parse the data due to its style (different culture)
+            dataTable.Columns.Add("Confirmed Last Week", typeof(int));      // 11
+            dataTable.Columns.Add("1 Week Change", typeof(int));            // 12
+            dataTable.Columns.Add("1 Week % Increase");                     // 13  Tried to define as double, but couldn't parse the data due to its style (different culture)
+            dataTable.Columns.Add("WHO Region");
+
+            string filePath = @"covid19.csv";
+            StreamReader streamReader = new StreamReader(filePath);
+
+            string[] totalData = new string[File.ReadAllLines(filePath).Length];
+            totalData = streamReader.ReadLine().Split(',');
+
+            while (!streamReader.EndOfStream)
+            {
+                totalData = streamReader.ReadLine().Split(',');
+                dataTable.Rows.Add(totalData[0], totalData[1], totalData[2], totalData[3], totalData[4], totalData[5], totalData[6], totalData[7], totalData[8], totalData[9], totalData[10], totalData[11], totalData[12], totalData[13], totalData[14]);
+            }
+
+            DataView dv = new DataView(dataTable);
+
+            //dv.RowFilter = string.Format("Country like '%{0}*%'", textBox_country.Text);
+
+            //dv.RowFilter = "CONVERT(Confirmed, 'System.Int32') > 10000";
+            //dv.RowFilter = "Confirmed <= {0}", Convert.ToInt32(textBox_maxConfirmed.Text);
+            //dv.RowFilter = "Confirmed >= {Convert.ToInt32(textBox_minConfirmed.Text)}";
+            dataGridView1.DataSource = dv;
+
+        }
+
+        private void button_show_Click(object sender, EventArgs e)
+        {
+            #region DATATABLE CREATING AGAIN :(
+
+            DataTable dataTable = new DataTable();
+
+            dataTable.Columns.Add("Country");                               // 0
+            dataTable.Columns.Add("Confirmed", typeof(int));                // 1
+            dataTable.Columns.Add("Deaths", typeof(int));                   // 2
+            dataTable.Columns.Add("Recovered", typeof(int));                // 3
+            dataTable.Columns.Add("Active", typeof(int));                   // 4
+            dataTable.Columns.Add("New Cases", typeof(int));                // 5
+            dataTable.Columns.Add("New Deaths", typeof(int));               // 6
+            dataTable.Columns.Add("New Recovered", typeof(int));            // 7
+            dataTable.Columns.Add("Deaths/100 cases");                      // 8   Tried to define as double, but couldn't parse the data due to its style (different culture)
+            dataTable.Columns.Add("Recovered/100 cases");                   // 9   Tried to define as double, but couldn't parse the data due to its style (different culture)
+            dataTable.Columns.Add("Deaths/100 Recovered");                  // 10  Tried to define as double, but couldn't parse the data due to its style (different culture)
+            dataTable.Columns.Add("Confirmed Last Week", typeof(int));      // 11
+            dataTable.Columns.Add("1 Week Change", typeof(int));            // 12
+            dataTable.Columns.Add("1 Week % Increase");                     // 13  Tried to define as double, but couldn't parse the data due to its style (different culture)
+            dataTable.Columns.Add("WHO Region");                            // 14
+
+            string filePath = @"covid19.csv";
+            StreamReader streamReader = new StreamReader(filePath);
+
+            string[] totalData = new string[File.ReadAllLines(filePath).Length];
+            totalData = streamReader.ReadLine().Split(',');
+
+            while (!streamReader.EndOfStream)
+            {
+                totalData = streamReader.ReadLine().Split(',');
+                if ( (Convert.ToInt32(totalData[1]) >= Convert.ToInt32(textBox_minConfirmed.Text)) && (Convert.ToInt32(totalData[1]) <= Convert.ToInt32(textBox_maxConfirmed.Text)) && (Convert.ToInt32(totalData[2]) <= Convert.ToInt32(textBox_maxDeaths.Text)) && (Convert.ToInt32(totalData[2]) >= Convert.ToInt32(textBox_minDeaths.Text)) && (Convert.ToInt32(totalData[3]) <= Convert.ToInt32(textBox_maxRecovered.Text)) && (Convert.ToInt32(totalData[3]) >= Convert.ToInt32(textBox_minRecovered.Text)))
+                {
+                    dataTable.Rows.Add(totalData[0], totalData[1], totalData[2], totalData[3], totalData[4], totalData[5], totalData[6], totalData[7], totalData[8], totalData[9], totalData[10], totalData[11], totalData[12], totalData[13], totalData[14]);
+                }
+            }
+
+            #endregion
+
+            DataView dv = new DataView(dataTable);
+            dv.RowFilter = string.Format("Country Like '{0}%'", textBox_country.Text);  // % operator was utilized for partial searching
+            //dv.RowFilter = "Confirmed < 1";  // % operator was utilized for partial searching
+            dataGridView1.DataSource = dv;
+
+
+
+
+        }
+
+        private void textBox_maxConfirmed_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
